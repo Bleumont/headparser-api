25531f0b247b247a287adb807a79741331e35559
Bleumont <fede.inch@gmail.com>
2021-01-17 22:41:20 -0300

    ipaddress typo

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/api/hello', function (req, res) {
 
 app.get('/api/whoami', (req, res) => {
   res.json({
-    ipadress: req.ip,
+    ipaddress: req.ip,
     language: req.headers['accept-language'],
     software: req.headers['user-agent'],
   });
